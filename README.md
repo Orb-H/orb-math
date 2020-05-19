debb064fc26f8a4a3380ee4d875cfd548533e139
Orb_H <orb.cov@gmail.com>
2020-05-19 20:11:44 +0900

    1-wn | add toString()s

--- README.md
+++ README.md
@@ -27,4 +27,4 @@ Repo for just a hope to make a mathematics library by implementing in a more mat
 - [x] 클래스의 구조
 - [x] 컴퓨터에서 사용하는 일반적인 정수형 값을 이 클래스로 만들어주는 함수(또는 생성자)
 - [ ] 문자열(hex, bin)을 파싱하여 이 클래스로 만들어주는 함수(또는 생성자)
-- [ ] 현재 클래스가 가지는 값(hex, bin)을 표시하는 함수
+- [x] 현재 클래스가 가지는 값(hex, bin)을 표시하는 함수
--- src/whole_number.cpp
+++ src/whole_number.cpp
@@ -5,4 +5,54 @@ using namespace orbmath;
 
 WholeNumber::WholeNumber(unsigned long long l) { data.push_back(l); }
 
+string WholeNumber::toBinString()
+{
+    string res;
+    unsigned long long l;
+    unsigned long long mask = 0x8000000000000000ULL;
+    char tmp;
+
+    res.append("0b");
+    for (int i = data.size() - 1; i >= 0; i--)
+    {
+        l = data[i];
+        for (int j = 0; j < 64; j++)
+        {
+            tmp = (char)(((l & mask) >> 63) & 0x1);
+            if (tmp > 0)
+                res += '1';
+            else if ((res.size() > 2) || (!i && j == 15))
+                res += '0';
+            l <<= 1;
+        }
+    }
+    return res;
+}
+
+string WholeNumber::toHexString()
+{
+    string res;
+    unsigned long long l;
+    unsigned long long mask = 0xf000000000000000ULL;
+    char tmp;
+
+    res.append("0x");
+    for (int i = data.size() - 1; i >= 0; i--)
+    {
+        l = data[i];
+        for (int j = 0; j < 16; j++)
+        {
+            tmp = (char)(((l & mask) >> 60) & 0xf);
+            if (tmp >= 10)
+                res += tmp - 10 + 'a';
+            else if (tmp > 0)
+                res += tmp + '0';
+            else if ((res.size() > 2) || (!i && j == 15))
+                res += '0';
+            l <<= 4;
+        }
+    }
+    return res;
+}
+
 WholeNumber WholeNumber::fromULL(unsigned long long l) { return WholeNumber(l); }
