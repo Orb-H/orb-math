4f4e3bae1cc9d2e76da0054e30e3dfeb126e2146
Orb_H <orb.cov@gmail.com>
2020-05-21 00:19:19 +0900

    1-wn | add constructors for string

--- src/whole_number.cpp
+++ src/whole_number.cpp
@@ -1,9 +1,154 @@
 #include "whole_number.h"
+#include <stdexcept>
 
 using namespace std;
 using namespace orbmath;
 
 WholeNumber::WholeNumber(unsigned long long l) { data.push_back(l); }
+
+WholeNumber::WholeNumber(string s, int radix)
+{
+    unsigned long long l;
+    switch (radix)
+    {
+    case 2:
+        if (s.substr(0, 2) == "0b")
+            s = s.substr(2);
+
+        while (s.length() > 64)
+        {
+            string t = s.substr(s.length() - 64);
+            s = s.substr(0, s.length() - 64);
+
+            l = 0;
+            for (int i = 0; i < 64; i++)
+            {
+                switch (t[i])
+                {
+                case '0':
+                case '1':
+                    l += ((unsigned long long)(t[i] - '0')) << (63 - i);
+                    break;
+                default:
+                    throw invalid_argument("Wrong character for binary integer");
+                }
+            }
+
+            data.push_back(l);
+        }
+        if (s.length() == 0)
+            throw invalid_argument("Empty string");
+
+        l = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            switch (s[i])
+            {
+            case '0':
+            case '1':
+                l += ((unsigned long long)(s[i] - '0')) << (s.length() - 1 - i);
+                break;
+            default:
+                throw invalid_argument("Wrong character for binary integer");
+            }
+        }
+        data.push_back(l);
+        break;
+    case 16:
+        if (s.substr(0, 2) == "0x")
+            s = s.substr(2);
+
+        while (s.length() > 16)
+        {
+            string t = s.substr(s.length() - 16);
+            s = s.substr(0, s.length() - 16);
+
+            l = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                switch (t[i])
+                {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    l += ((unsigned long long)(t[i] - '0')) << (4 * (15 - i));
+                    break;
+                case 'a':
+                case 'b':
+                case 'c':
+                case 'd':
+                case 'e':
+                case 'f':
+                    l += ((unsigned long long)(t[i] - 'a' + 10)) << (4 * (15 - i));
+                    break;
+                case 'A':
+                case 'B':
+                case 'C':
+                case 'D':
+                case 'E':
+                case 'F':
+                    l += ((unsigned long long)(t[i] - 'A' + 10)) << (4 * (15 - i));
+                    break;
+                default:
+                    throw invalid_argument("Wrong character for hexadecimal integer");
+                }
+            }
+
+            data.push_back(l);
+        }
+        if (s.length() == 0)
+            throw invalid_argument("Empty string");
+
+        l = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            switch (s[i])
+            {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                l += ((unsigned long long)(s[i] - '0')) << (4 * (s.length() - 1 - i));
+                break;
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
+                l += ((unsigned long long)(s[i] - 'a' + 10)) << (4 * (s.length() - 1 - i));
+                break;
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            case 'F':
+                l += ((unsigned long long)(s[i] - 'A' + 10)) << (4 * (s.length() - 1 - i));
+                break;
+            default:
+                throw invalid_argument("Wrong character for hexadecimal integer");
+            }
+        }
+        data.push_back(l);
+        break;
+    default:
+        throw invalid_argument("Invalid radix");
+    }
+}
 
 string WholeNumber::toBinString()
 {
@@ -56,3 +201,7 @@ string WholeNumber::toHexString()
 }
 
 WholeNumber WholeNumber::fromULL(unsigned long long l) { return WholeNumber(l); }
+
+WholeNumber WholeNumber::fromBinString(string bin) { return WholeNumber(bin, 2); }
+
+WholeNumber WholeNumber::fromHexString(string hex) { return WholeNumber(hex, 16); }
